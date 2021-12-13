fe21fe417f89c41e8098c68f83da8590249ee758
Benjamin Bellamy <benjamin@bellamy.ovh>
2021-12-13 15:32:50 +0100

    Update log4j_checker_beta.sh

--- log4j_checker_beta.sh
+++ log4j_checker_beta.sh
@@ -3,6 +3,10 @@
 # Run with:
 # wget https://raw.githubusercontent.com/ad-aures/log4j_checker_beta/main/log4j_checker_beta.sh -q -O - |bash
 
+echo "Install locate…"
+sudo apt-get -qq update
+sudo apt-get -qq -y install locate
+
 echo "checking for log4j vulnerability...";
 if [ "$(locate log4j|grep -v log4js)" ]; then
   echo "### maybe vulnerable, those files contain the name:";
