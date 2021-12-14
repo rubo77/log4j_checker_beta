2f37258f33732f935e2013f300435396d5965035
rubo77 <github@r.z11.de>
2021-12-14 07:19:25 +0100

    Don't check for log4js* and log4j_checker_beta itself and enhance warnings

--- log4j_checker_beta.sh
+++ log4j_checker_beta.sh
@@ -42,7 +42,7 @@ function find_jar_files() {
 }
 
 information "Looking for files containing log4j..."
-OUTPUT="$(locate_log4j | grep -v log4js)"
+OUTPUT="$(locate_log4j | grep -iv log4js | grep -v log4j_checker_beta)"
 if [ "$OUTPUT" ]; then
   warning "Maybe vulnerable, those files contain the name:"
   printf "%s\n" "$OUTPUT"
@@ -50,7 +50,7 @@ fi
 
 if [ "$(command -v yum)" ]; then
   information "Checking installed yum packages..."
-  OUTPUT="$(yum list installed | grep -i log4j | grep -v log4js)"
+  OUTPUT="$(yum list installed | grep -i log4j | grep -iv log4js)"
 
   if [ "$OUTPUT" ]; then
     warning "Maybe vulnerable, yum installed packages:"
@@ -60,7 +60,7 @@ fi
 
 if [ "$(command -v dpkg)" ]; then
   information "Checking installed dpkg packages..."
-  OUTPUT="$(dpkg -l | grep -i log4j | grep -v log4js)"
+  OUTPUT="$(dpkg -l | grep -i log4j | grep -iv log4js)"
   if [ "$OUTPUT" ]; then
     warning "Maybe vulnerable, dpkg installed packages:"
     printf "%s\n" "$OUTPUT"
@@ -72,8 +72,8 @@ JAVA="$(command -v java)"
 if [ "$JAVA" ]; then
   warning "Java is installed"
   printf "     %s\n     %s\n" \
-    "Java applications often bundle their libraries inside jar/war/ear files," \
-    "so there still could be log4j in such applications."
+    "Java applications often bundle their libraries inside binary files," \
+    "so there could be log4j in such applications."
 else
   information "Java is not installed"
 fi
@@ -90,8 +90,9 @@ else
 fi
 
 information "_________________________________________________"
-echo "If you see no uncommented output above this line, you are safe. Otherwise check the listed files and packages.";
 if [ "$JAVA" == "" ]; then
-  echo "Some apps bundle the vulnerable library in their own compiled package, so 'java' might not be installed but one such apps could still be vulnerable."
+  warning "Some apps bundle the vulnerable library in their own compiled package, so 'java' might not be installed but one such apps could still be vulnerable."
 fi
-printf "\nNote: this is not 100% proof you are not vulnerable, but a strong hint!\n"
+echo
+warning "This whole script is not 100% proof you are not vulnerable, but a strong hint"
+echo
