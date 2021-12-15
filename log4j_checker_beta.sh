08b1ad8cb136813bc69417b2fca4b67d89f66ed5
Thomas Dankert <thomas.dankert@stihl.de>
2021-12-15 10:07:47 +0100

    Refactored code (log messages) and check .ear and .war as well

--- log4j_checker_beta.sh
+++ log4j_checker_beta.sh
@@ -8,46 +8,102 @@
 
 # optionally scans the entire disk (using ionice)
 
+# regular expression, check the following packages
+PACKAGES='solr\|elastic\|log4j'
+
+# IO load definition (1 = realtime, 2 = best-effort (priority 0-7), 3 = idle)
+IONICE='ionice -c 2 -n 7'
+
 RED="\033[0;31m"; GREEN="\033[32m"; YELLOW="\033[1;33m"; ENDCOLOR="\033[0m"
-WARNING="[WARNING]"${ENDCOLOR}
 
-# check using locate
-echo -e ${YELLOW}"### locate files containing log4j ..."${ENDCOLOR1}
-OUTPUT="$(locate -e log4j|grep -v log4js|grep -v log4j_checker_beta)"
+LANG=
+
+function warning() {
+  printf "${RED}[WARNING] %s${ENDCOLOR}\n" "$1" >&2
+}
+
+function information() {
+  printf "${YELLOW}[INFO] %s${ENDCOLOR}\n" "$1"
+}
+
+function ok() {
+  printf "${GREEN}[INFO] %s${ENDCOLOR}\n" "$1"
+}
+
+function locate_log4j() {
+  if [ "$(command -v locate)"]; then
+    information "Using locate, which may use an outdated database. Please run updatedb to refresh it."
+    locate log4j
+  fi
+}
+
+function scan_filesystem() {
+  $IONICE find / -iname log4j-core-*.jar 2>&1 \
+    | grep -v '^find:.* Permission denied$' \
+    | grep -v '^find:.* No such file or directory$'
+}
+
+function scan_in_archive_files() {
+  $IONICE find / -iname "*.jar" -o -iname "*.war" -o -iname "*.ear" \
+  -exec sh -c 'unzip -l {}|grep -H --label {} JndiLookup.class' \; \
+  2>&1 \
+    | grep -v '^find:.* Permission denied$' \
+    | grep -v '^find:.* No such file or directory$'
+}
+
+# check root user
+if [ $USER != root ]; then
+  warning "Please run this script as the root user, otherwise not all files will be found."
+fi
+
+# first scan: use locate
+echo
+information "Looking for files containing log4j..."
+OUTPUT="$(locate_log4j | grep -iv log4js | grep -v log4j_checker_beta)"
 if [ "$OUTPUT" ]; then
-  echo -e ${RED}"[WARNING] maybe vulnerable, those files contain the name:"${ENDCOLOR}
-  echo "$OUTPUT"
-fi;
+  warning "Maybe vulnerable, found log4j in the following files:"
+  printf "%s\n" "$OUTPUT"
+else
+  ok "No files containing log4j"
+fi
 
-# check using yum (if installed)
+# second scan: use package manager
+echo
+information "Checking installed packages: ($PACKAGES)"
 if [ "$(command -v yum)" ]; then
-  echo -e ${YELLOW}"### check installed yum packages ..."${ENDCOLOR1}
-  OUTPUT="$(yum list installed|grep log4j|grep -v log4js)"
+  # using yum
+  OUTPUT="$(yum list installed | grep -i $PACKAGES | grep -iv log4js)"
   if [ "$OUTPUT" ]; then
-    echo -e ${RED}"[WARNING] maybe vulnerable, yum installed packages:"${ENDCOLOR}
-    echo "$OUTPUT"
-  fi;
-fi;
-
-# check using dpkg (if installed)
+    warning "Maybe vulnerable, yum installed packages:"
+    printf "%s\n" "$OUTPUT"
+  else
+    ok "No yum packages found"
+  fi
+fi
 if [ "$(command -v dpkg)" ]; then
-  echo -e ${YELLOW}"### check installed dpkg packages ..."${ENDCOLOR1}
-  OUTPUT="$(dpkg -l|grep log4j|grep -v log4js)"
+  # using dpkg
+  OUTPUT="$(dpkg -l | grep -i $PACKAGES | grep -iv log4js)"
   if [ "$OUTPUT" ]; then
-    echo -e ${RED}"[WARNING] maybe vulnerable, dpkg installed packages:"${ENDCOLOR}
-    echo "$OUTPUT"
-  fi;
-fi;
+    warning "Maybe vulnerable, dpkg installed packages:"
+    printf "%s\n" "$OUTPUT"
+  else
+    ok "No dpkg packages found"
+  fi
+fi
 
-# check for java
-echo -e ${YELLOW}"### check if Java is installed ..."${ENDCOLOR1}
+# third scan: check for "java" command
+echo
+information "Checking if Java is installed..."
 JAVA="$(command -v java)"
 if [ "$JAVA" ]; then
-  echo -e ${RED}"[WARNING] Java is installed"${ENDCOLOR}
-  echo "Java applications often bundle their libraries inside jar/war/ear files, so there still could be log4j in such applications.";
+  warning "Java is installed"
+  printf "     %s\n     %s\n" \
+    "Java applications often bundle their libraries inside binary files," \
+    "so there could be log4j in such applications."
 else
-  echo -e ${GREEN}"[OK]"${ENDCOLOR}" Java is not installed"
-fi;
+  ok "Java is not installed"
+fi
+
 
 # ask for confirmation for filesystem scan
 echo 
@@ -55,26 +111,28 @@ read -p "Do you want to scan the filesystem for log4j-core-*.jar and JdniLookup.
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
   # perform best-effort (lowest priority) find call for log4j-core jar
-  echo -e ${YELLOW}"### checking filesystem for log4j-core-*.jar ..."${ENDCOLOR1}
-  OUTPUT="$(ionice -c 2 -n 7 find / -name log4j-core-*.jar 2>/dev/null)"
+  information "Checking filesystem for log4j-core-*.jar..."
+  OUTPUT="$(scan_filesystem)"
   if [ "$OUTPUT" ]; then
-    echo -e ${RED}"[WARNING] maybe vulnerable, those files contain the log4j jar:"${ENDCOLOR}
-    echo "$OUTPUT"
-  fi;
+    warning "Maybe vulnerable, found the log4j jar in the following files:"
+    printf "%s\n" "$OUTPUT"
+  fi
 
   # perform best-effort find call for all jars, and search for JndiLookup.class inside
-  echo -e ${YELLOW}"### checking filesystem for 'JndiLookup.class' in *.jar ..."${ENDCOLOR1}
-  OUTPUT="$(ionice -c 2 -n 7 find / -name "*.jar" -exec sh -c 'unzip -l {}|grep -H --label {} JndiLookup.class' \; 2>/dev/null)"
-    if [ "$OUTPUT" ]; then
-    echo -e ${RED}"[WARNING] maybe vulnerable, those files contain the JndiLookup.class:"${ENDCOLOR}
-    echo "$OUTPUT"
-  fi;
-fi;
-
-echo -e ${YELLOW}"_________________________________________________"${ENDCOLOR}
-echo "If you see no uncommented output above this line, you are safe. Otherwise check the listed files and packages.";
-if [ "$JAVA" == "" ]; then
-  echo "Some apps bundle the vulnerable library in their own compiled package, so 'java' might not be installed but one such apps could still be vulnerable."
+  echo
+  information "Analyzing JAR/WAR/EAR files..."
+  OUTPUT="$(scan_in_archive_files)"
+  if [ "$OUTPUT" ]; then
+    warning "Maybe vulnerable, the following files contain the JndiLookup.class:"
+    printf "%s\n" "$OUTPUT"
+  fi
 fi
+
+information "_________________________________________________"
+if [ "$JAVA" == "" ]; then
+  warning "Some apps bundle the vulnerable library in their own compiled package, so even if 'java' is not installed, one of the applications could still be vulnerable."
+fi
+
 echo
-echo "Note, this is not 100% proof you are not vulnerable, but a strong hint!"
+warning "This script does not guarantee that you are not vulnerable, but is a strong hint."
+echo
